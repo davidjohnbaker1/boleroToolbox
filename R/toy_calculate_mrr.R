4addc6aa6a25e95adfdd4877871e0ccc0cd1e6e4
davidjohnbaker1 <davidjohnbaker1@gmail.com>
2025-01-08 14:28:03 -0500

    package dev starts here

--- R/toy_calculate_mrr.R
+++ R/toy_calculate_mrr.R
@@ -1,13 +1,14 @@
 #' toy_calculate_mrr
 #'
-#' @param n_customers
-#' @param product_price
+#' @param n_customers The Number of Customers
+#' @param product_price How Much You Might Charge
 #'
-#' @return mrr
+#' @return ans
 #' @export
 #'
 #' @examples
 #' toy_calculate_mrr(n_customers = 10, product_price = 300)
 toy_calculate_mrr <- function(n_customers, product_price) {
-  n_customers * product_price
+  ans <- n_customers * product_price
+  return(ans)
 }
